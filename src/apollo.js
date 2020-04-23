3e103054683ec7f7d0ddf5d65cb0c8e5593f1de6
reviday <reviday@naver.com>
2020-04-23 14:48:59 +0900

    #1.1 GET_MOVIES Query

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,8 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri: "https://movieql.now.sh"
+  // uri: "https://movieql.now.sh", // 현재는 운영 종료
+  uri: "http://localhost:4000" // movieql 레포지토리를 직접 로컬에서 실행. 이 프로젝트를 실행하기 위해서는 movieql도 실행시켜줘야 함.
 });
 
 export default client;
